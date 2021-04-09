88bd7f6c383b55a84a87d0b494ad424c2977db39
tz8 <tz8@iu.edu>
2021-04-08 21:35:29 -0400

    milestone 4 finished

--- The Lost Darkness/Assets/Scripts/PlayerMovement.cs
+++ The Lost Darkness/Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,27 @@ using UnityEngine.SceneManagement;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private LayerMask platformLayerMask;
+
+    public AudioClip jumpVoice;
+    public AudioClip jumpSFX;
+    public AudioClip footStep1;
+    public AudioClip footStep2;
+    public AudioClip footStep3;
+    public AudioClip footStep4;
+
     public float movementSpeed = 6f;
     public float jumpForce = 10f;
 
     private float horizontalMovement;
     private Rigidbody2D body;
     private BoxCollider2D collider;
+    AudioSource audioSource;
 
     private void Start()
     {
         body = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -31,6 +41,11 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         body.velocity = new Vector2(horizontalMovement * movementSpeed, body.velocity.y);
+
+        if(IsGrounded() && body.velocity.x != 0f && !audioSource.isPlaying)
+        {
+            playFootSteps();
+        }
     }
 
     private void Update()
@@ -39,7 +54,36 @@ public class PlayerMovement : MonoBehaviour
 
         if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
+            audioSource.PlayOneShot(jumpVoice, 1.0f);
+            audioSource.PlayOneShot(jumpSFX, 1.0f);
             body.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void playFootSteps()
+    {
+        int clipChoice = Random.Range(0, 4);
+        switch (clipChoice)
+        {
+            case 0:
+                audioSource.PlayOneShot(footStep1, 1.0f);
+                break;
+
+            case 1:
+                audioSource.PlayOneShot(footStep2, 1.0f);
+                break;
+
+            case 2:
+                audioSource.PlayOneShot(footStep3, 1.0f);
+                break;
+
+            case 3:
+                audioSource.PlayOneShot(footStep4, 1.0f);
+                break;
+
+            default:
+                audioSource.PlayOneShot(footStep1, 1.0f);
+                break;
         }
     }
 
